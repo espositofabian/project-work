988fff313dc479c1864bd10111e1f02d13f4f1bd
Fabian Esposito <fabian.esposito@outlook.com>
2025-02-04 12:35:46 +0100

    Update Risposte.sql
    Aggiunta colonna a domande

--- Risposte.sql
+++ Risposte.sql
@@ -2,10 +2,10 @@
 ---- Trovare 5 Domande e Risposte per livello di difficoltà. Da 1 a 15. Quindi 75 domande totali.
 
 insert into domande 
-(id,q,punti)
+(id,q,punti,argomento)
 values
-	(1,"Come si chiama lo stregone amico degli Hobbit ne Il signore degli anelli?",1),
-	(2,"Chi interpreta l'agente Rick Deckard in Blade Runner?",1);
+	(1,"Come si chiama lo stregone amico degli Hobbit ne Il signore degli anelli?",1,"Generico"),
+	(2,"Chi interpreta l'agente Rick Deckard in Blade Runner?",1,"Generico");
 
 insert into risposte 
 	(id,risi,ris2,ris3,ris4,risok, idDomanda)
