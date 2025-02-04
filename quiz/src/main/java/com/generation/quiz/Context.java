5f8999075c17138df3fd2d6c6faec4e6cda80a6d
martinaals <martinalaserra2001@gmail.com>
2025-02-04 16:50:48 +0100

    Classe Context
    Creazione classe Context e riativazione dei costruttori vuoti di Entity e Utente

---
+++ quiz/src/main/java/com/generation/quiz/Context.java
@@ -0,0 +1,58 @@
+package com.generation.quiz;
+import java.util.Map;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import com.generation.dao.Database;
+import com.generation.quiz.entities.Entity;
+import com.generation.quiz.entities.Utente;
+
+@Configuration
+public class Context {
+	
+	@Bean 
+	public Database db()
+	{
+		return new Database();
+	}
+	
+	@Bean
+	public DaoUtenti du()
+	{
+		return new DaoUtenti();
+	}
+	
+	@Bean
+	@Scope("prototype")
+	public Entity mappaUtente(Map<String,String> mappa)
+	{
+		Entity e = new Utente();
+
+		if(e instanceof Utente)
+		{
+			e.setId(Integer.parseInt(mappa.get("id")));
+			Utente u = (Utente) e;
+			u.setUsername(mappa.get("username"));
+			u.setPassword(mappa.get("password"));
+		}
+
+		return e;
+	}
+	
+	// Per creare oggetti Utente dal sito 
+	@Bean
+	@Scope("prototype")
+	public Utente oggettoUtente(String username,String password)
+	{
+		Utente u = new Utente();
+		
+		u.setId(0);
+		u.setUsername(username);
+		u.setPassword(password);
+		
+		return u;
+	}
+	
+}
