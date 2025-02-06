118601792f7d4e6115b817c9d0a4b0c961439dc1
Alessio230801 <alessioporri@gmail.com>
2025-02-06 12:53:39 +0100

    Update query ordinata bella e pulita.sql

--- query ordinata bella e pulita.sql
+++ query ordinata bella e pulita.sql
@@ -239,9 +239,16 @@ create table utenti
 insert into utenti
 (id,username,password)
 values
-(1,"Anna","annaanna"),
-(2,"Luca","Garmin"),
-(3,"Manuel","Unity");
+(1,"Anna","annaanna",0),
+(2,"Luca","Garmin",100),
+(3,"Manuel","Unity",10),
+(4,"Lucian","George",50),
+(5,"Andrea","Macerata",25),
+(6,"Giordano","Zuppli",5),
+(7,"Dylan","SSNapoli1926",300),
+(8,"Vincent","javaaaaaaaaaaaaaaaaaa",5),
+(9,"DJFalco","CostaRica",5),
+(10,"Gulshan","SonoUnIndianoMalefico",5);
 
 -- da qui in poi tutte query utili più o meno, Luca è l'essere perfetto
 select	* from	domande;
